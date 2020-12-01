2576ec9fe47f4454cda8f8afee53e4ff99eecb76
Bert <55228359+1800yungbert@users.noreply.github.com>
2020-12-01 05:08:14 -0500

    Add project files.

---
+++ BertPog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BertPog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.WriteLine($"Nice to meet you, {Console.ReadLine()}!");
+
+            Console.ReadLine();
+        }
+    }
+}
